837a2274ca76bad685900aaca141d80cfb6f8d88
Adrian Palcic <adrian.palcic@gmail.com>
2025-07-27 00:26:22 +0200

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+
+
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-[#5542ff] py-4 text-black items-center flex flex-col justify-center">
+<div className="w-full flex align-middle justify-center overflow-hidden">
+  <h1 className="text-[clamp(2rem,20vw,40rem)] leading-[1] tracking-[0.1em] text-center uppercase font-bold ml-5">
+    Zentry
+  </h1>
+</div>    
+   <p>©Zentry 2024. All rights reserved</p>
+    </footer>
+  );
+};
+
+export default Footer;
